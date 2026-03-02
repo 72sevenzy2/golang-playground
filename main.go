ff2eece6cd7f701a38f5cd5ce10d1b209b45ba72
72 <72sevenzy2@gmail.com>
2026-03-02 15:00:37 +0500

    practiced with how interfaces are satisfied once again.

--- main.go
+++ main.go
@@ -1,82 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// func divide(a, b int) (int, error) {
-// 	if b == 0 || a == 0 {
-// 		return 0, fmt.Errorf("cannot divide by zero")
-// 	}
-// 	return a / b, nil
-// }
-
-// exercise 4
-type Greeter interface {
-	greet() string
+type incrementer interface {
+	increment()
 }
 
-type Person struct {
-	Name string
-}
+type counter int
 
-func (p *Person) greet() string{
-	 return "person name " + p.Name
-}
-
-type Bot struct {
-	ID int
-}
-
-func (b Bot) greet() string{
-	return fmt.Sprintf("bot id", b.ID)
-}
-
-func sayHello(g Greeter) {
-	fmt.Println(g.greet())
+func (c *counter) increment() {
+	*c++
 }
 
 func main() {
-	p := Person{Name: "cool"}
-	a := Bot{ID: 5}
+	var i incrementer
 
-	sayHello(&p)
-	sayHello(a)
-
-	// first exercise
-	// a := []int{10, 20, 30, 40}
-	// b := a[1:3]
-	// b[0] = 99
-
-	// c := make([]int, len(b))
-	// copy(c, b)
-	// b[1] = 100
-
-	// fmt.Println(a) // 10, 99, 100, 40
-	// fmt.Println(b) // 99, 100
-	// fmt.Println(c) // 99, 30
-
-	// second exercise
-	// result, err := divide(10, 5)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Println(result)
-
-	// var i Incrementer
-
-	// c := Counter(5)
-	// i = &c
-	// i.int()
-	// fmt.Println(c)
+	c := counter(0)
+	i = &c
+	i.increment()
+	fmt.Println(c)
 }
-
-// exercise 3
-// type Counter int
-
-// type Incrementer interface {
-// 	int()
-// }
-
-// func (c *Counter) int() {
-// 	*c++
-// }
