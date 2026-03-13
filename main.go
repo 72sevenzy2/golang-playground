7f66e4b8f650feb920862c8a367504fb91044671
72 <72sevenzy2@gmail.com>
2026-03-13 16:37:53 +0500

    tested forloops, for recalling

--- main.go
+++ main.go
@@ -2,19 +2,10 @@ package main
 
 import "fmt"
 
-var arr [3][2]int
-
 func main() {
-	arr[1][0] = 99
-	fmt.Println(arr[1][0])
-	fmt.Println(arr)
+	var b [3]int
 
-	// seperate from this exercise ^^
-
-	var i counter = 0
-
-	var b incrementer = &i
-
-	b.increase()
-	
+	for range len(b) {
+		fmt.Println("hi")
+	}
 }
