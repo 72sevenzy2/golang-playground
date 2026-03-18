1999b5656ce7d0430494387ff2d19e8491c32314
72 <72sevenzy2@gmail.com>
2026-03-18 15:16:37 +0500

    worked with buffered channels

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import "fmt"
 
-func send(ch chan<- int) {
-	ch <- 6
-}
+// func send(ch chan<- int) {
+// 	ch <- 6
+// }
 
-func receive(ch <-chan int) {
-	fmt.Println(<-ch)
-}
+// func receive(ch <-chan int) {
+// 	fmt.Println(<-ch)
+// }
 
 func main() {
-	ch := make(chan int)
-	go send(ch)
-	receive(ch)
+	ch := make(chan int, 2)
+
+	ch <- 2
+	ch <- 1
+
+	fmt.Println(<-ch)
 }
