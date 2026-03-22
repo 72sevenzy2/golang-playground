238db4372e844badc31218485c9c644cbd62c8b6
72 <72sevenzy2@gmail.com>
2026-03-22 10:39:45 +0500

    added some comments for my own explanation

--- main.go
+++ main.go
@@ -6,7 +6,8 @@ import (
 )
 
 func worker(id int, results chan<- int, jobs <-chan int, su *sync.WaitGroup) {
-	defer su.Done()
+	// this decreases the count by 1, so the loop i added is range 3 so the number of workers started will be 3 along with the count in the sync wg, but this will decrease it by 1 untill its 0 each time the func is called, which is 3 due to the loop.
+	defer su.Done() 
 
 	for i := range jobs{
 		results <- i * 2
@@ -21,6 +22,8 @@ func main() {
 
 	var b sync.WaitGroup
 
+
+	// gorountine to add 1 to the sync waitgroup and start the number of workers as needed
 	for z := range 3 {
 		b.Add(1)
 		go worker(z, results, jobs, &b)
@@ -32,7 +35,9 @@ func main() {
 		}
 		close(jobs)
 	}()
+  
 
+	// different gorountine to close the channel results ONLY after workers are done
 	go func ()  {
 		b.Wait()
 		close(results)
