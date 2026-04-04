e3df066d3e55b42fdad5a338d76bcfe84d313fbb
72 <72sevenzy2@gmail.com>
2026-04-04 13:59:53 +0500

    testing channels:

--- main.go
+++ main.go
@@ -2,48 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-func worker(id int, results chan<- int, jobs <-chan int, su *sync.WaitGroup) {
-	// this decreases the count by 1, so the loop i added is range 3 so the number of workers started will be 3 along with the count in the sync wg, but this will decrease it by 1 untill its 0 each time the func is called, which is 3 due to the loop.
-	defer su.Done() 
-
-	for i := range jobs{
-		results <- i * 2
+// worker function
+func worker(id int, tasks <-chan int, results chan<- int) {
+	for task := range tasks {
+		fmt.Println("Worker", id, "processing", task)
+		results <- task * 2 // example processing
 	}
-
-	fmt.Println("workers done", id)
 }
 
 func main() {
-	jobs := make(chan int)
-	results := make(chan int)
+	tasks := make(chan int, 5)
+	results := make(chan int, 5)
 
-	var b sync.WaitGroup
-
-
-	// gorountine to add 1 to the sync waitgroup and start the number of workers as needed
-	for z := range 3 {
-		b.Add(1)
-		go worker(z, results, jobs, &b)
+	// start 3 workers
+	for i := 1; i <= 3; i++ {
+		go worker(i, tasks, results)
 	}
 
-	go func() {
-		for i := range 3 {
-			jobs <- i
-		}
-		close(jobs)
-	}()
-  
+	tasks <- 1
+	tasks <- 2
+	close(tasks)
 
-	// different gorountine to close the channel results ONLY after workers are done
-	go func ()  {
-		b.Wait()
-		close(results)
-	}()
-
-	for res := range results {
-		fmt.Println("workers done: ", res)
+	// collect results
+	for i := 1; i <= 2; i++ {
+		fmt.Println("Result:", <-results)
 	}
 }
