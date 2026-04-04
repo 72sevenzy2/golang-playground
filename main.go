7afded472f2fae300d9ed89e1c2a6a4b4a54be5e
72 <72sevenzy2@gmail.com>
2026-04-04 16:19:11 +0500

    tested go scheduler behaviour

--- main.go
+++ main.go
@@ -1,36 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func worker(id int, rec chan<- int, sen <-chan int) {
-	fmt.Println("worker id", id)
-	for i := range sen {
-		fmt.Println("received with id", i)
-		rec <- i
-	}
-}
+import "fmt"
 
 func main() {
 	ch1 := make(chan int)
-	rec := make(chan int)
-
-	for i := 0; i < 3; i++ {
-		time.Sleep(time.Second)
-		go worker(i, rec, ch1)
-	}
+	ch2 := make(chan int)
 
 	go func ()  {
 		ch1 <- 1
-		ch1 <- 2
-		ch1 <- 3
-		close(ch1)
 	}()
 
-	for i := 1; i < 4; i++ {
-		fmt.Println(<-rec)
-	}
+	go func ()  {
+		ch2 <- 2
+	}()
 
+	go func ()  {
+		ch1 <- 4
+	}()
+
+	go func ()  {
+		ch1 <- 7
+	}()
+
+	go func ()  {
+		ch2 <- 3
+	}()
+
+	for range 2 {
+		fmt.Println(<-ch1)
+		fmt.Println(<-ch2)
+	}
 }
