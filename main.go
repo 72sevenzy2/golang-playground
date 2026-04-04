a35f1dc746d4980f206a6db20d31fce9a5036d8c
72 <72sevenzy2@gmail.com>
2026-04-04 14:57:24 +0500

    added time delay to force all workers to run

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func worker(id int, rec chan<- int, sen <-chan int) {
+	fmt.Println("worker id", id)
 	for i := range sen {
 		fmt.Println("received with id", i)
 		rec <- i
@@ -15,7 +17,8 @@ func main() {
 	ch1 := make(chan int)
 	rec := make(chan int)
 
-	for i := 1; i < 3; i++ {
+	for i := 0; i < 3; i++ {
+		time.Sleep(time.Second)
 		go worker(i, rec, ch1)
 	}
 
