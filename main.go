ca56f8a0c2811b7a6f9758831ccb304feae83abc
72 <72sevenzy2@gmail.com>
2026-04-05 14:34:54 +0500

    test simple send/receive exercise

--- main.go
+++ main.go
@@ -5,30 +5,17 @@ import (
 	"time"
 )
 
-func worker(id int, ch1 <-chan int, ch2 chan<- int) {
-	fmt.Println("go worker")
-	for i := range ch1 {
-		fmt.Println("received with id", id)
-		ch2 <- i
-	}
-}
-
 func main() {
-	ch1 := make(chan int) // sender
-	ch2 := make(chan int) // receiver
-
-	for i := range 4 {
-		time.Sleep(time.Second)
-		go worker(i, ch1, ch2)
-	}
+	ch1 := make(chan int)
 
 	go func ()  {
 		ch1 <- 1
 		ch1 <- 2
-		ch2 <- 3
+		ch1 <- 3
 	}()
 
 	for range 3 {
-		fmt.Println(<-ch2)
+		time.Sleep(time.Second)
+		fmt.Println(<-ch1)
 	}
 }
