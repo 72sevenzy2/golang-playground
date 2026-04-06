842e7ec2c405d99d32aaa92f536ddef7cf0c8145
72 <72sevenzy2@gmail.com>
2026-04-06 14:26:54 +0500

    test worker pool again

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
+// simple worker pool
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func worker(id int, tasks <-chan int, results chan<- int) {
+	for i := range tasks {
+		fmt.Println("received task from worker", id)
+		results <- i
+	}
+}
 
 func main() {
-	ch := make(chan int, 2)
+	tasksch := make(chan int, 2)
+	resch := make(chan int, 2)
 
-	ch <- 1
-	ch <- 2
+	go func ()  {
+		tasksch <- 1
+		tasksch <- 2
+		close(tasksch)
+	}()
 
-	for range 2 {
-		fmt.Println(<-ch)
+	for i := range 3 {
+		time.Sleep(time.Second)
+		go worker(i, tasksch, resch)
 	}
+
+	fmt.Println(<-resch)
+	fmt.Println(<-resch)
 }
