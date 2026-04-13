b60cf87c4aeaef1121f6d8c514ce71e2c163a1fd
72 <72sevenzy2@gmail.com>
2026-04-13 19:23:10 +0500

    working on fan-in pattern

--- main.go
+++ main.go
@@ -2,57 +2,23 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-type task struct {
-	index int
-	value int 
-}
 
-type result struct {
-	index int
-	value int
-}
 
-func worker(wg *sync.WaitGroup, tasks <-chan task, results chan<- result) {
-	defer wg.Done()
-	for i := range tasks {
-		fmt.Println("received task number:", i)
-		results <- result{index: i.index, value: i.value * 2} // example tasks
+// fan in pattern
+
+func worker(id int, res chan<- string) {
+	for range 3 {
+		res <- fmt.Sprintf("received worker with id %d", id)
 	}
+	close(res)
 }
 
 func main() {
-	tasks := make(chan task, 5)
-	results := make(chan result, 5) // buffered channels = queue
+	ch1 := make(chan string) // receiver
+	ch2 := make(chan string) // tasks
 
-	var wg sync.WaitGroup
-
-	for range 3 {
-		wg.Add(1)
-		go worker(&wg, tasks, results)
-	}
-
-	go func() {
-		for i := range 3 {
-			tasks <- task{index: i, value: i}
-		}
-		close(tasks)
-	}()
-
-	go func ()  {
-		wg.Wait()
-		close(results)
-	}()
-
-	output := make([]int, 3)
-
-	for v := range results {
-		output[v.index] = v.value
-	}
-
-	for _, v := range output {
-		fmt.Println(v)
-	}
+	go worker(1, ch1)
+	go worker(1, ch2)
 }
