6bdcccc4aa0016c91ae3b270d6263e5914e18c73
72 <72sevenzy2@gmail.com>
2026-04-14 16:47:53 +0500

    added simple context example

--- main.go
+++ main.go
@@ -1,43 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"sync"
+	"time"
 )
-
-func worker(wg *sync.WaitGroup, id int, results chan<- int, tasks <-chan int) {
-	defer wg.Done()
-	fmt.Println("received work with id", id)
-	for i := range tasks {
-		fmt.Println("task id", i)
-		results <- i
+ 
+func worker(ctx context.Context) {
+	for {
+		select {
+		case <- ctx.Done():
+			fmt.Println("stopped working")
+			return
+		
+		case <- time.After(1 * time.Second):
+			fmt.Println("working..")
+		}
 	}
 }
 
 func main() {
-	tasks := make(chan int)
-	results := make(chan int)
+	ctx, cancel := context.WithCancel(context.Background())
 
-	var wg sync.WaitGroup
+	go worker(ctx)
 
-	for i := range 5 {
-		wg.Add(1)
-		go worker(&wg, i, results, tasks)
-	}
+	time.Sleep(3 * time.Second)
+	cancel()
 
-	go func ()  {
-		for i := range 3 {
-			tasks <- i
-		}
-		close(tasks)
-	}()
-
-	go func ()  {
-		wg.Wait()
-		close(results)
-	}()
-
-	for range results {
-		fmt.Println(<-results)
-	}
+	time.Sleep(500 * time.Millisecond)
 }
